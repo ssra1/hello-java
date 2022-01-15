1e1a5810a420a24da03a50fcfa62edb7c3874dd5
seshaseyanareddy <seshaseyanareddy@gmail.com>
2022-01-15 06:14:38 +0000

    code updated in helloworld class file

--- helloworld.java
+++ helloworld.java
@@ -21,8 +21,8 @@ public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello World, DevOps has arrived!" to the terminal window.
         System.out.println("Hello World, DevOps has arrived!");
-System.out.println("Hello World, DevOps has arrived!");
-System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        
 
     }
 
