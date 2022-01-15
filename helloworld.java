99ecaea611a4a9d86cb9337493c4d5ce68de8557
seshaseyanareddy <seshaseyanareddy@gmail.com>
2022-01-15 06:06:26 +0000

    multiple lines are added in helloworldjava class

--- helloworld.java
+++ helloworld.java
@@ -21,6 +21,9 @@ public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello World, DevOps has arrived!" to the terminal window.
         System.out.println("Hello World, DevOps has arrived!");
+System.out.println("Hello World, DevOps has arrived!");
+System.out.println("Hello World, DevOps has arrived!");
+
     }
 
 }
